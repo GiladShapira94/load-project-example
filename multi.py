df277cd87e97c334ab77af967840f109a0d4dc9e
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-07-24 10:57:31 +0300

    Update and rename taxi.py to multi.py

---
+++ multi.py
@@ -0,0 +1,11 @@
+def multi_3(event):
+    event['int']=event['int']*3
+    return event
+
+def multi_4(event):
+    event['int']=event['int']*4
+    return event
+
+def multi_5(event):
+    event['int']=event['int']*5
+    return event
