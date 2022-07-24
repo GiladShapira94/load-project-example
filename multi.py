2cd9c2c8bb119424788a6c85c10cf95b241b1ecf
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-07-24 11:22:30 +0300

    Update multi.py

--- multi.py
+++ multi.py
@@ -1,11 +1,14 @@
 def multi_3(event):
     event['int']=event['int']*3
+    print(event)
     return event
 
 def multi_4(event):
     event['int']=event['int']*4
+    print(event)
     return event
 
 def multi_5(event):
     event['int']=event['int']*5
+    print(event)
     return event
