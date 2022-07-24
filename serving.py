a92f1ff46100937f93c4ec25e79185357a2826d5
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-07-24 12:19:36 +0300

    Update serving.py

--- serving.py
+++ serving.py
@@ -1,21 +1,15 @@
-import numpy as np
-import mlrun
-import os
-from cloudpickle import load
-from mlrun.artifacts import get_artifact_meta
-class LGBMModel(mlrun.serving.V2ModelServer):
-    
-    def load(self):
-        model_file, extra_data = self.get_model('.pkl')
-        self.model_meta = get_artifact_meta(self.model_path)[0]
-        self.model = load(open(model_file, 'rb'))
+def start(event):
+    print("Start")
+    return event
 
-    def predict(self, body):
-        try:
-#             print("model_hash",self.model_meta.hash)
-            feats = np.asarray(body['inputs'])
-            result = self.model.predict(feats)
-            response = [result.tolist(),f"Model Hash {self.model_meta.hash}"]
-            return response
-        except Exception as e:
-            raise Exception("Failed to predict %s" % e)
+def double(event):
+    return event * 2
+
+
+def add3(event):
+    return event + 3
+
+
+def end(event):
+    print("End")
+    return event
