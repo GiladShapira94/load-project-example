8037eef453d457dc8a277813ff4fec3cee696d55
GiladShapira94 <giladsh@iguazio.com>
2022-08-03 10:49:42 +0000

    Update Files

--- .ipynb_checkpoints/my_job_with_requirements-checkpoint.py
+++ .ipynb_checkpoints/my_job_with_requirements-checkpoint.py
@@ -3,5 +3,5 @@ from mlrun import MLClientCtx
 
 def log_df(context:MLClientCtx,df=None):
     if df==None:
-        df=.pd.Data
+        df=pd.DataFrame({"col1":[1,2,3,4,5]})
     context.log_dataset(key='df1',df=df,format='csv')
